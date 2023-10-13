4f1227c6176c71fa808dec3ecee43ac3509c5666
Mshujon <mshujon@MKs-MacBook-Pro.local>
2023-10-13 12:53:30 +0200

    body margin correction

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -18,7 +18,7 @@ const Home = () => {
                 <h1 className="text-3xl font-bold my-5">¡Oh My Quiz¡ - Test Your <br /> Frontend Skill</h1>
                 <p className="text-xl">If you get a high score, you probably spend way too much time on Learning <br /> this Languages. Keep Learning.</p>
             </div>
-           <div className="flex justify-between mx-20 mb-20 md:max-xl:grid grid-cols-2 gap-10 mx-0! max-md:flex-col mb-5! ">
+           <div className='xl:flex justify-between mx-20 mb-20 md:max-xl:grid grid-cols-2 gap-10 mx-0!  max-md:flex max-md:flex-col max-md:mx-auto max-md:mb-5'>
                 {
                     quizs.map(quiz => <Quiz image ={quiz.logo} name ={quiz.name} id={quiz.id} total = {quiz.total} key={quiz.id} > </Quiz> )
 
